aa2c3b1a295f4c9470a46b98412786d9b04fa33b
vladimir <edoomik@gmail.com>
2016-04-20 18:58:44 +0300

    implemented CRUD functionality

---
+++ app/resource/model.py
@@ -0,0 +1,32 @@
+import settings
+from pymongo import ReturnDocument
+from pymongo.results import InsertOneResult
+from bson import json_util
+from app.database import database
+from bson.objectid import ObjectId
+
+class ModelResult(object):
+    def __init__(self, result):
+        self.result = result
+
+    def to_json(self):
+        if isinstance(self.result, InsertOneResult):
+            return json_util.dumps(self.result.inserted_id)
+        return json_util.dumps(self.result)
+
+class ResourceModel(object):
+    def __init__(self):
+        self.collection = database[settings.MONGODB_COLLECTION_RESOURCE]
+
+    def get_all_resources(self):
+        return ModelResult(self.collection.find())
+
+    def create(self, endpoint, methods, response):
+        return ModelResult(self.collection.insert_one({'endpoint': endpoint, 'methods': methods, 'response': response}))
+
+    def delete(self, resource_id):
+        self.collection.remove({'_id': ObjectId(resource_id)})
+
+    def patch(self, resource_id, properties):
+        properties = {'$set': properties}
+        return ModelResult(self.collection.find_one_and_update({'_id': ObjectId(resource_id)}, properties, return_document=ReturnDocument.AFTER))
--- app/resource/resource.py
+++ app/resource/resource.py
@@ -1,8 +1,52 @@
-from flask import Blueprint, jsonify
+import json
+from flask import Blueprint, request, Response
+from model import ResourceModel
 
+
+HTTP_OK = 200
+HTTP_BAD_REQUEST = 400
 
 blueprint = Blueprint('resource', __name__)
+resource_model = ResourceModel()
 
-@blueprint.route('/')
-def get_resource():
-    return jsonify(status="OK")
+def select_from(target, what):
+    properties = {key: val for key, val in target.iteritems() if key in what}
+    return properties
+
+@blueprint.route('/resource', methods=['GET'])
+def get_resources_list():
+    return Response(
+        response=resource_model.get_all_resources().to_json(),
+        status=HTTP_OK,
+        mimetype='application/json'
+    )
+
+@blueprint.route('/resource', methods=['POST'])
+def create_new_resource():
+    payload = request.json
+    payload['methods'].sort()
+    new_resource = resource_model.create(payload['endpoint'], payload['methods'], payload['response'])
+    return Response(
+        response=new_resource.to_json(),
+        status=HTTP_OK,
+        mimetype='application/json'
+    )
+
+@blueprint.route('/resource/<string:resource_id>', methods=['DELETE'])
+def delete_resource(resource_id):
+    resource_model.delete(resource_id)
+    return Response(
+        response = '{}',
+        status=HTTP_OK,
+        mimetype='application/json'
+    )
+
+@blueprint.route('/resource/<string:resource_id>', methods=['PATCH'])
+def patch_resource(resource_id):
+    properties_to_update = select_from(request.json, ['endpoint', 'methods', 'response'])
+    patched_resource = resource_model.patch(resource_id, properties_to_update)
+    return Response(
+        response=patched_resource.to_json(),
+        status=HTTP_OK,
+        mimetype='application/json'
+    )
