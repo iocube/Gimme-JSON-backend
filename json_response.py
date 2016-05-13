0c8a47a757653df4cbc9ef8588569325cf71f053
Vladimir <edoomik@gmail.com>
2016-05-13 10:59:11 +0300

    removed code snippet

--- json_response.py
+++
@@ -1,8 +0,0 @@
-def json_response(f):
-    def json_wrapper(*args, **kwargs):
-        wrapped_response = f(*args, **kwargs)
-        wrapped_response.mimetype = 'application/json'
-        if wrapped_response.response and hasattr(wrapped_response.response, 'to_json'):
-            wrapped_response.set_data(wrapped_response.response.to_json())
-        return wrapped_response
-    return json_wrapper
