22a06ae943996ba4091ab56d7dad3533d5c35fd8
Vladimir <edoomik@gmail.com>
2016-05-13 19:21:19 +0300

    added 'test' command to run unit tests

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 import pymongo
 import json
+import subprocess
+
 from settings import settings
 from gimmejson import application
 from flask.ext.script import Manager, Server
@@ -8,6 +10,7 @@ from app.database import database, connection
 
 manager = Manager(application)
 database_manager = Manager()
+tests_manager = Manager()
 
 @database_manager.command
 def createindexes():
@@ -23,6 +26,27 @@ def populate():
     resources = json.loads(f.read())
     f.close()
     database[settings.MONGODB_COLLECTION_RESOURCE].insert_many(resources)
+
+@manager.option('-m', '--module', dest='module_name')
+@manager.option('-c', '--class', dest='class_name')
+@manager.option('-s', '--singletest', dest='test_name')
+def test(module_name=None, class_name=None, test_name=None):
+    """
+    Run tests.
+    NOTE: modules have to be prefixed with 'test_'.
+    """
+    if module_name and (not class_name and not test_name):
+        # equivalent to running $ python -m <package>.<module>
+        subprocess.call(['python', '-m', 'tests.{m}'.format(m=module_name)])
+    elif module_name and class_name and not test_name:
+        # equivalent to running $ python -m <package>.<module> <class>
+        subprocess.call(['python', '-m', 'tests.{m}'.format(m=module_name), '{c}'.format(c=class_name)])
+    elif module_name and class_name and test_name:
+        # equivalent to running $ python -m <package>.<module> <class>.<test>
+        subprocess.call(['python', '-m', 'tests.{m}'.format(m=module_name), '{c}.{t}'.format(c=class_name, t=test_name)])
+    else:
+        # run all tests
+        subprocess.call(['python', '-m', 'unittest', 'discover', '-s', 'tests'])
 
 manager.add_command("runserver", Server(use_debugger=True, use_reloader=True))
 manager.add_command("database", database_manager)
