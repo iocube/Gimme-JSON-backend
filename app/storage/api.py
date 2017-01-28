822ed30f49c8a1e015134d507a31d52ea328a686
Vladimir Zeifman <edoomik@gmail.com>
2017-01-28 18:01:28 +0200

    90 - implemented PUT for `storage/<id>` endpoint
    - PUT on `storage/<id>` will save updated version of storage entity

--- app/storage/api.py
+++ app/storage/api.py
@@ -5,7 +5,7 @@ from app import decorators
 from app.storage import serializers
 from app.exceptions import raise_validation_error, raise_not_found
 from app.storage.dao import StorageDAO
-from app.error_messages import ERR_EMPTY_PAYLOAD, ERR_DUPLICATE_VALUE
+from app.error_messages import ERR_EMPTY_PAYLOAD, ERR_DUPLICATE_VALUE, ERR_NOTHING_TO_UPDATE
 
 storage = StorageDAO()
 
@@ -64,3 +64,26 @@ def delete(storage_id):
         return {}
 
     return raise_not_found()
+
+@decorators.crossdomain()
+@decorators.to_json
+@decorators.jwt_auth_required
+def save(storage_id):
+    incoming_json = request.get_json(silent=True) or raise_validation_error(
+        non_field_errors=[ERR_EMPTY_PAYLOAD]
+    )
+
+    incoming_storage, error = serializers.Storage(exclude=('_id',)).load(incoming_json)
+    if error:
+        raise_validation_error(error)
+    elif not incoming_storage:
+        raise_validation_error(non_field_errors=[ERR_NOTHING_TO_UPDATE])
+
+    single_storage = storage.get_by_id(storage_id)
+    if not single_storage:
+        raise_not_found()
+
+    saved_storage = storage.save(storage_id, incoming_storage)
+
+    serialized = serializers.Storage().dump(saved_storage)
+    return serialized.data
