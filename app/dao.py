d25f20cb290ee9838666c0a210f59f567a50cb93
Vladimir Zeifman <edoomik@gmail.com>
2017-01-28 17:54:33 +0200

    methods of  `BaseDAO` can be accept other values  as their `_id`
    - `__parse_document_id()` will determine if given document_id can be converted to ObjectId, in that case it will create and return it

--- app/dao.py
+++ app/dao.py
@@ -7,12 +7,9 @@ class BaseDAO(object):
         self.collection = None
 
     def get_by_id(self, document_id):
-        id_to_find = document_id
-
-        if ObjectId.is_valid(document_id):
-            id_to_find = ObjectId(id_to_find)
-
-        return self.collection.find_one({'_id': id_to_find})
+        return self.collection.find_one({
+            '_id': self.__parse_document_id(document_id)
+        })
 
     def get_all(self):
         return self.collection.find()
@@ -24,21 +21,25 @@ class BaseDAO(object):
         return document
 
     def delete(self, document_id):
-        if ObjectId.is_valid(document_id):
-            return self.collection.find_one_and_delete({'_id': ObjectId(document_id)})
-
-        return self.collection.find_one_and_delete({'_id': document_id})
+        return self.collection.find_one_and_delete(
+            {'_id': self.__parse_document_id(document_id)}
+        )
 
     def save(self, document_id, updated_document):
         return self.collection.find_one_and_replace(
-            {'_id': ObjectId(document_id)},
+            {'_id': self.__parse_document_id(document_id)},
             updated_document,
             return_document=ReturnDocument.AFTER
         )
 
     def update(self, document_id, partial_document):
         return self.collection.find_one_and_update(
-            {'_id': ObjectId(document_id)},
+            {'_id': self.__parse_document_id(document_id)},
             {'$set': partial_document},
             return_document=ReturnDocument.AFTER
         )
+
+    def __parse_document_id(self, document_id):
+        if ObjectId.is_valid(document_id):
+            return ObjectId(document_id)
+        return document_id
