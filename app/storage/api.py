94130d2d945c03479694b5ec3466f16ac685238b
Vladimir Zeifman <edoomik@gmail.com>
2017-01-31 21:45:29 +0200

    refactored storage/api.py  to use class based views

--- app/storage/api.py
+++ app/storage/api.py
@@ -1,7 +1,8 @@
 from flask import request
+from flask.views import MethodView
 from pymongo.errors import DuplicateKeyError
 
-from app import decorators
+from app.decorators import crossdomain, to_json, jwt_auth_required
 from app.storage import serializers
 from app.exceptions import raise_validation_error, raise_not_found
 from app.storage.dao import StorageDAO
@@ -9,81 +10,79 @@ from app.error_messages import ERR_EMPTY_PAYLOAD, ERR_DUPLICATE_VALUE, ERR_NOTHI
 
 storage = StorageDAO()
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def get_list():
-    storage_list = storage.get_all()
-    serialized = serializers.Storage(many=True).dump(storage_list)
-    return serialized.data
 
+class StorageCollection(MethodView):
+    decorators = [
+        jwt_auth_required,
+        to_json,
+        crossdomain()
+    ]
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def get(storage_id):
-    single_storage = storage.get_by_id(storage_id)
-    if single_storage:
-        serialized = serializers.Storage().dump(single_storage)
+    def get(self):
+        storage_list = storage.get_all()
+        serialized = serializers.Storage(many=True).dump(storage_list)
         return serialized.data
 
-    return raise_not_found()
+    def post(self):
+        incoming_json = request.get_json(silent=True) or raise_validation_error(
+            non_field_errors=[ERR_EMPTY_PAYLOAD]
+        )
+
+        data, error = serializers.Storage().load(incoming_json)
+        if error:
+            raise_validation_error(error)
+
+        try:
+            new_storage = storage.create(**data)
+        except DuplicateKeyError:
+            field = 'id'
+            raise_validation_error(field_errors={
+                field: ERR_DUPLICATE_VALUE.format(field='id')
+            })
+
+        serialized = serializers.Storage().dump(new_storage)
+        return serialized.data
 
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def create():
-    incoming_json = request.get_json(silent=True) or raise_validation_error(
-        non_field_errors=[ERR_EMPTY_PAYLOAD]
-    )
+class StorageEntity(MethodView):
+    decorators = [
+        jwt_auth_required,
+        to_json,
+        crossdomain()
+    ]
 
-    data, error = serializers.Storage().load(incoming_json)
-    if error:
-        raise_validation_error(error)
+    def get(self, storage_id):
+        single_storage = storage.get_by_id(storage_id)
+        if single_storage:
+            serialized = serializers.Storage().dump(single_storage)
+            return serialized.data
 
-    try:
-        new_storage = storage.create(**data)
-    except DuplicateKeyError:
-        field = 'id'
-        raise_validation_error(field_errors={
-            field: ERR_DUPLICATE_VALUE.format(field='id')
-        })
+        return raise_not_found()
 
-    serialized = serializers.Storage().dump(new_storage)
-    return serialized.data
+    def delete(self, storage_id):
+        deleted = storage.delete(storage_id)
 
+        if deleted:
+            return {}
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def delete(storage_id):
-    deleted = storage.delete(storage_id)
+        return raise_not_found()
 
-    if deleted:
-        return {}
+    def put(self, storage_id):
+        incoming_json = request.get_json(silent=True) or raise_validation_error(
+            non_field_errors=[ERR_EMPTY_PAYLOAD]
+        )
 
-    return raise_not_found()
+        incoming_storage, error = serializers.Storage(exclude=('_id',)).load(incoming_json)
+        if error:
+            raise_validation_error(error)
+        elif not incoming_storage:
+            raise_validation_error(non_field_errors=[ERR_NOTHING_TO_UPDATE])
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def save(storage_id):
-    incoming_json = request.get_json(silent=True) or raise_validation_error(
-        non_field_errors=[ERR_EMPTY_PAYLOAD]
-    )
+        single_storage = storage.get_by_id(storage_id)
+        if not single_storage:
+            raise_not_found()
 
-    incoming_storage, error = serializers.Storage(exclude=('_id',)).load(incoming_json)
-    if error:
-        raise_validation_error(error)
-    elif not incoming_storage:
-        raise_validation_error(non_field_errors=[ERR_NOTHING_TO_UPDATE])
+        saved_storage = storage.save(storage_id, incoming_storage)
 
-    single_storage = storage.get_by_id(storage_id)
-    if not single_storage:
-        raise_not_found()
-
-    saved_storage = storage.save(storage_id, incoming_storage)
-
-    serialized = serializers.Storage().dump(saved_storage)
-    return serialized.data
+        serialized = serializers.Storage().dump(saved_storage)
+        return serialized.data
--- app/storage/routes.py
+++ app/storage/routes.py
@@ -4,8 +4,5 @@ from flask import Blueprint
 
 blueprint = Blueprint('storage', __name__)
 
-blueprint.add_url_rule('/storage/', view_func=api.get_list, methods=['GET'])
-blueprint.add_url_rule('/storage/<string:storage_id>', view_func=api.get, methods=['GET'])
-blueprint.add_url_rule('/storage/', view_func=api.create, methods=['POST'])
-blueprint.add_url_rule('/storage/<string:storage_id>', view_func=api.delete, methods=['DELETE'])
-blueprint.add_url_rule('/storage/<string:storage_id>', view_func=api.save, methods=['PUT'])
+blueprint.add_url_rule('/storage/', view_func=api.StorageCollection.as_view('storage_collection'))
+blueprint.add_url_rule('/storage/<string:storage_id>', view_func=api.StorageEntity.as_view('storage_entity'))
