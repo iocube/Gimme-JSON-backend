5c3db0c4527961f56e1747f0e57a2e4023c7b19a
Vladimir Zeifman <edoomik@gmail.com>
2017-02-01 21:26:23 +0200

    refactored endpoint/api.py  to use class based views

--- app/endpoint/api.py
+++ app/endpoint/api.py
@@ -1,7 +1,8 @@
 from flask import request
+from flask.views import MethodView
 from pymongo.errors import DuplicateKeyError
 
-from app import decorators
+from app.decorators import to_json, crossdomain, jwt_auth_required
 from app.endpoint import serializers
 from app.exceptions import raise_validation_error, raise_not_found
 from app.endpoint.dao import EndpointDAO
@@ -11,118 +12,110 @@ from app.error_messages import ERR_EMPTY_PAYLOAD, ERR_NOTHING_TO_UPDATE, ERR_DUP
 endpoint = EndpointDAO()
 
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def get(endpoint_id):
-    single_endpoint = endpoint.get_by_id(endpoint_id)
-    if single_endpoint:
-        serialized = serializers.Endpoint().dump(single_endpoint)
+class EndpointCollection(MethodView):
+    decorators = [
+        jwt_auth_required,
+        to_json,
+        crossdomain()
+    ]
+
+    def get(self):
+        endpoint_list = endpoint.get_all()
+        serialized = serializers.Endpoint(many=True).dump(endpoint_list)
         return serialized.data
 
-    return raise_not_found()
+    def create(self):
+        incoming_json = request.get_json(silent=True) or raise_validation_error(
+            non_field_errors=[ERR_EMPTY_PAYLOAD]
+        )
+
+        data, error = serializers.Endpoint(exclude=('_id',)).load(incoming_json)
+        if error:
+            raise_validation_error(field_errors=error)
+
+        try:
+            new_endpoint = endpoint.create(**data)
+        except DuplicateKeyError:
+            field = 'route'
+            raise_validation_error(field_errors={
+                field: ERR_DUPLICATE_VALUE.format(field=field)
+            })
+
+        serialized = serializers.Endpoint().dump(new_endpoint)
+        return serialized.data
 
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def get_list():
-    endpoint_list = endpoint.get_all()
-    serialized = serializers.Endpoint(many=True).dump(endpoint_list)
-    return serialized.data
+class EndpointEntity(MethodView):
+    decorators = [
+        jwt_auth_required,
+        to_json,
+        crossdomain()
+    ]
 
+    def get(self, endpoint_id):
+        single_endpoint = endpoint.get_by_id(endpoint_id)
+        if single_endpoint:
+            serialized = serializers.Endpoint().dump(single_endpoint)
+            return serialized.data
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def create():
-    incoming_json = request.get_json(silent=True) or raise_validation_error(
-        non_field_errors=[ERR_EMPTY_PAYLOAD]
-    )
+        return raise_not_found()
 
-    data, error = serializers.Endpoint(exclude=('_id',)).load(incoming_json)
-    if error:
-        raise_validation_error(field_errors=error)
+    def delete(self, endpoint_id):
+        if not is_object_id_valid(endpoint_id):
+            raise_not_found()
 
-    try:
-        new_endpoint = endpoint.create(**data)
-    except DuplicateKeyError:
-        field = 'route'
-        raise_validation_error(field_errors={
-            field: ERR_DUPLICATE_VALUE.format(field=field)
-        })
+        deleted = endpoint.delete(endpoint_id)
 
-    serialized = serializers.Endpoint().dump(new_endpoint)
-    return serialized.data
+        if deleted:
+            return {}
 
+        return raise_not_found()
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def delete(endpoint_id):
-    if not is_object_id_valid(endpoint_id):
-        raise_not_found()
+    def put(self, endpoint_id):
+        if not is_object_id_valid(endpoint_id):
+            raise_not_found()
 
-    deleted = endpoint.delete(endpoint_id)
+        incoming_json = request.get_json(silent=True) or raise_validation_error(
+            non_field_errors=[ERR_EMPTY_PAYLOAD]
+        )
 
-    if deleted:
-        return {}
+        updated_endpoint, error = serializers.Endpoint(exclude=('_id',)).load(incoming_json)
 
-    return raise_not_found()
+        if error:
+            raise_validation_error(field_errors=error)
+        elif not updated_endpoint:
+            raise_validation_error(non_field_errors=[ERR_NOTHING_TO_UPDATE])
 
+        try:
+            updated_endpoint = endpoint.save(endpoint_id, updated_endpoint)
+        except DuplicateKeyError:
+            field = 'route'
+            raise_validation_error(field_errors={
+                field: ERR_DUPLICATE_VALUE.format(field=field)
+            })
 
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def partial_update(endpoint_id):
-    if not is_object_id_valid(endpoint_id):
-        raise_not_found()
+        serialized = serializers.Endpoint().dump(updated_endpoint)
+        return serialized.data
 
-    incoming_json = request.get_json(silent=True) or raise_validation_error(
-        non_field_errors=[ERR_EMPTY_PAYLOAD]
-    )
+    def patch(self, endpoint_id):
+        if not is_object_id_valid(endpoint_id):
+            raise_not_found()
 
-    fields_to_update, error = serializers.Endpoint(exclude=('_id',), partial=True).load(incoming_json)
-    if error:
-        raise_validation_error(error)
-    elif not fields_to_update:
-        raise_validation_error(non_field_errors=[ERR_NOTHING_TO_UPDATE])
+        incoming_json = request.get_json(silent=True) or raise_validation_error(
+            non_field_errors=[ERR_EMPTY_PAYLOAD]
+        )
 
-    try:
-        patched_endpoint = endpoint.update(endpoint_id, fields_to_update)
-    except DuplicateKeyError:
-        field = 'route'
-        raise_validation_error(field_errors={field: ERR_DUPLICATE_VALUE.format(field=field)})
+        fields_to_update, error = serializers.Endpoint(exclude=('_id',), partial=True).load(incoming_json)
+        if error:
+            raise_validation_error(error)
+        elif not fields_to_update:
+            raise_validation_error(non_field_errors=[ERR_NOTHING_TO_UPDATE])
 
-    serialized = serializers.Endpoint().dump(patched_endpoint)
-    return serialized.data
+        try:
+            patched_endpoint = endpoint.update(endpoint_id, fields_to_update)
+        except DuplicateKeyError:
+            field = 'route'
+            raise_validation_error(field_errors={field: ERR_DUPLICATE_VALUE.format(field=field)})
 
-
-@decorators.crossdomain()
-@decorators.to_json
-@decorators.jwt_auth_required
-def save(endpoint_id):
-    if not is_object_id_valid(endpoint_id):
-        raise_not_found()
-
-    incoming_json = request.get_json(silent=True) or raise_validation_error(
-        non_field_errors=[ERR_EMPTY_PAYLOAD]
-    )
-
-    updated_endpoint, error = serializers.Endpoint(exclude=('_id',)).load(incoming_json)
-
-    if error:
-        raise_validation_error(field_errors=error)
-    elif not updated_endpoint:
-        raise_validation_error(non_field_errors=[ERR_NOTHING_TO_UPDATE])
-
-    try:
-        updated_endpoint = endpoint.save(endpoint_id, updated_endpoint)
-    except DuplicateKeyError:
-        field = 'route'
-        raise_validation_error(field_errors={
-            field: ERR_DUPLICATE_VALUE.format(field=field)
-        })
-
-    serialized = serializers.Endpoint().dump(updated_endpoint)
-    return serialized.data
+        serialized = serializers.Endpoint().dump(patched_endpoint)
+        return serialized.data
--- app/endpoint/routes.py
+++ app/endpoint/routes.py
@@ -4,9 +4,5 @@ from flask import Blueprint
 
 blueprint = Blueprint('endpoint', __name__)
 
-blueprint.add_url_rule('/endpoint/', view_func=api.get_list, methods=['GET'])
-blueprint.add_url_rule('/endpoint/', view_func=api.create, methods=['POST'])
-blueprint.add_url_rule('/endpoint/<string:endpoint_id>/', view_func=api.get, methods=['GET'])
-blueprint.add_url_rule('/endpoint/<string:endpoint_id>/', view_func=api.delete, methods=['DELETE'])
-blueprint.add_url_rule('/endpoint/<string:endpoint_id>/', view_func=api.partial_update, methods=['PATCH'])
-blueprint.add_url_rule('/endpoint/<string:endpoint_id>/', view_func=api.save, methods=['PUT'])
+blueprint.add_url_rule('/endpoint/', view_func=api.EndpointCollection.as_view('endpoint_collection'))
+blueprint.add_url_rule('/endpoint/<string:endpoint_id>/', view_func=api.EndpointEntity.as_view('endpoint_entity'))
